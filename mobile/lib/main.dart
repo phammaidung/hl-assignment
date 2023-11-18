65853ae1731f2fc375881aa97134cde5d29eae16
phammaidung <phammaidung8@gmail.com>
2023-11-19 06:38:55 +0700

    hide checked mode banner

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -18,6 +18,7 @@ class JokeApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: JokeScreen(),
     );
   }
