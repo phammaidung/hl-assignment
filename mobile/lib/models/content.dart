c73ab2c12026cf42d2ea1cd3e623626dc09cd51a
phammaidung <phammaidung8@gmail.com>
2023-11-19 06:23:22 +0700

    adding id for jokes content to store db

--- mobile/lib/models/content.dart
+++ mobile/lib/models/content.dart
@@ -1,4 +1,18 @@
+import 'dart:convert';
+
+import 'package:crypto/crypto.dart';
+import 'package:uuid/uuid.dart';
+
+const uuid = Uuid();
+
 class Content {
-  const Content(this.content);
+  Content({required this.content, this.id});
+  String? id;
   final String content;
+}
+
+String generateId(String content) {
+  final idToByte = utf8.encode(content);
+  final contentId = sha256.convert(idToByte);
+  return contentId.toString();
 }
